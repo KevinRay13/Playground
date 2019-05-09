cda00263ef4cebac7799819c4dcce2851f32d926
Kevin Edmondson <kevinraye13@gmail.com>
2019-05-09 13:11:48 -0500

    initial layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <h1> Playground </h1>
       </header>
+      <body className='contentBox'>
+        <div className='box'>
+          <h2>box1</h2>
+        </div>
+        <div className='box'>
+          <h2>box2</h2>
+        </div>
+        <div className='box'>
+          {' '}
+          <h2>box3</h2>{' '}
+        </div>
+        <div className='box'>
+          {' '}
+          <h2>box4</h2>
+        </div>
+      </body>
     </div>
   );
 }

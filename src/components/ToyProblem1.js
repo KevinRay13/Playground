251816f63e6868255ff28ac1817e44a821b335ac
Kevin Edmondson <kevinraye13@gmail.com>
2019-05-10 15:00:37 -0500

    started vowel counter

--- src/components/ToyProblem1.js
+++ src/components/ToyProblem1.js
@@ -5,9 +5,27 @@ class ToyProblem1 extends Component {
     super();
     this.state = {};
   }
+  getCount(str) {
+    var vowelsCount = 0;
+
+    vowelsCount = str.match(/[aeiou]/g);
+    console.log(vowelsCount == null ? 0 : vowelsCount.length);
+    return vowelsCount == null ? 0 : vowelsCount.length;
+  }
 
   render() {
-    return <div>TP1</div>;
+    return (
+      <div>
+        <h3>Vowel Counter</h3>
+        <form action=''>
+          <input type='text' />
+          <input
+            type='submit'
+            onClick={() => this.getCount(this.target.value)}
+          />
+        </form>
+      </div>
+    );
   }
 }
 export default ToyProblem1;

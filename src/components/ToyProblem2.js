636eaf261bdb520e4abccaa21927c9929d030459
Kevin Edmondson <kevinraye13@gmail.com>
2019-05-14 23:42:07 -0500

    millenial bot

--- src/components/ToyProblem2.js
+++ src/components/ToyProblem2.js
@@ -4,16 +4,34 @@ import '../App.css';
 class ToyProblem2 extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      value: ''
+    };
   }
   //   function disemvowel(str) {
   //     let newStr = str.match(/[b-d, f-h, j-n, p-t, v-z, ?!.]/gi);
 
   //    return newStr.join('');
   //  }
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
 
   render() {
-    return <div />;
+    return (
+      <div>
+        <form onSubmit='' className='VCCont'>
+          <input
+            type='text'
+            value={this.state.value}
+            onChange={event => this.handleChange(event)}
+            className='VCInput'
+          />
+          <input type='submit' className='VCSubmit' />
+        </form>
+        <h3 className='VCCount' />
+      </div>
+    );
   }
 }
 export default ToyProblem2;
